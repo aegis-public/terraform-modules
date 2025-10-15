649929fd14f2cc05ff77d0df05a1ace016723ae8
Anukul Sangwan <anukul@aegisai.ai>
2025-10-15 12:45:25 -0400

    bump chart to 0.1.16

--- byoc-gcp/helm.tf
+++ byoc-gcp/helm.tf
@@ -46,7 +46,7 @@ resource "helm_release" "workspace_connector" {
   name             = local.helm_release_name
   repository       = "https://aegis-public.github.io/helm-charts"
   chart            = "workspace-connector"
-  version          = "0.1.15"
+  version          = "0.1.16"
   namespace        = var.kubernetes_namespace
   create_namespace = true
 
