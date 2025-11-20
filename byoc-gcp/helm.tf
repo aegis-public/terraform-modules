a720e67049f52bfe63830ba45e15ec5ee06de32f
Sam Malik <43284714+SRM47@users.noreply.github.com>
2025-11-20 10:31:21 -0800

    Update helm.tf

--- byoc-gcp/helm.tf
+++ byoc-gcp/helm.tf
@@ -71,7 +71,7 @@ resource "helm_release" "workspace_connector" {
   name             = local.helm_release_name
   repository       = "https://aegis-public.github.io/helm-charts"
   chart            = "workspace-connector"
-  version          = "0.1.23"
+  version          = "0.1.24-alpha"
   namespace        = var.kubernetes_namespace
   create_namespace = true
 
